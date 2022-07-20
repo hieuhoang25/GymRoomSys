7af56e826879f6918e0a101e838fe2a901dc14bc
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2022-07-20 22:10:21 +0700

    test validation

--- src/test/java/com/group2/dao/NhanVienDAOIT.java
+++ src/test/java/com/group2/dao/NhanVienDAOIT.java
@@ -7,11 +7,8 @@ package com.group2.dao;
 
 import com.group2.entity.NhanVien;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -22,22 +19,7 @@ public class NhanVienDAOIT {
     
     public NhanVienDAOIT() {
     }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
+
 
     /**
      * Test of insert method, of class NhanVienDAO.
