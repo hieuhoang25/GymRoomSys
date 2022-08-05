a6066d181a5f54f8e382b564dba3cdbf6e305649
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2022-08-05 18:28:25 +0700

    adcode

--- src/test/java/com/group2/swing/CheckInTest.java
+++ src/test/java/com/group2/swing/CheckInTest.java
@@ -4,13 +4,34 @@
  */
 package com.group2.swing;
 
+import com.group2.config.ReportTest;
+import com.group2.config.TestCase;
 import com.group2.ui.DangNhapJDialog;
 import com.group2.utils.Auth;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.api.Assertions;
+
 
 /**
  *
@@ -19,43 +40,90 @@ import org.junit.Test;
 public class CheckInTest {
 
     public static DangNhapJDialog dangNhapJDialog;
+   static ArrayList<TestCase> list = new ArrayList<>();
 
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
-//        dangNhapJDialog = new DangNhapJDialog();
-//        dangNhapJDialog.dangNhap("NV01", "123456");
+     
     }
 
-    @AfterClass
-    public static void tearDownClass() {
+    @AfterAll
+    public static void tearDownClass()  {
+        try {
+            ReportTest.writeExcel(list, "src/books.xlsx", "Chức năng check in");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 
-    @Before
+    @BeforeEach
     public void setUp() {
         dangNhapJDialog = new DangNhapJDialog();
-        dangNhapJDialog.dangNhap("NV01", "123456");
+        dangNhapJDialog.dangNhapToApp("NV01", "123456");
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         Auth.clear();
     }
-
-    @Test
-    public void testCheckInNotHV() {
-        System.out.println("checkIn");
-        String input = "0769089058";
-        Status instance = new Status();
-        instance.checkIn(input);
-    }
     
-     @Test
-    public void testCheckInIsHV() {
+    
+    @ParameterizedTest
+    @CsvFileSource(resources = "/login.csv",numLinesToSkip = 1)
+   
+    public void testCheckInNotHV(String input, String expected) {
         System.out.println("checkIn");
-        String input = "0776274144";
+       
         Status instance = new Status();
-        instance.checkIn(input);
-
+        
+        try {
+        	if (expected.equalsIgnoreCase("false")) {
+        		 Assertions.assertFalse(instance.checkin(input));
+        	}
+        	else {
+        		Assertions.assertTrue(instance.checkin(input));
+        	}
+        
+			
+          list.add( new TestCase("TC1","Check in hội viên với số điện thoại không "
+                    + "phải là hội viên",input,"Thông bá́o: \"Số điện thoại không phải của hội viên\"","Pass"));
+        } catch (Exception e) {
+           list.add(new TestCase("TC1","Check in hội viên với số điện thoại không phải là hội viên",input,"Thông bá́o: \"Số điện thoại không phải của hội viên\"","Fail"));
+       
+        }
     }
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/login.csv",numLinesToSkip = 1)
+//    
+//    public void testCheckInIsHV(String input) {
+//        System.out.println("checkIn");
+//       
+//        Status instance = new Status();
+//        
+//        try {
+//         Assertions.assertTrue(instance.checkin(input));
+//			
+//          list.add( new TestCase("TC1","Check in hội viên với số điện thoại không "
+//                    + "phải là hội viên",input,"Thông bá́o: \"Số điện thoại không phải của hội viên\"","Pass"));
+//        } catch (Exception e) {
+//           list.add(new TestCase("TC1","Check in hội viên với số điện thoại không phải là hội viên",input,"Thông bá́o: \"Số điện thoại không phải của hội viên\"","Fail"));
+//       
+//        }
+//    }
+//    @Test
+//    public void testCheckInIsHV() {
+//        System.out.println("checkIn");
+//        String input = "0776274144";
+//        Status instance = new Status();
+//        try {
+//            Assert.assertTrue(instance.checkin(input));
+//            
+//        list.add( new TestCase("TC2","Check in hội viên với số điện thoại là hội viên","sdt:0776274144","Thông báo: \"Check in thành công\"","Pass"));
+//        } catch (Exception e) {
+//         list.add (new TestCase("TC2","Check in hội viên với số điện thoại là hội viên","sdt:0776274144","Thông báo: \"Check in thành công\"","Fail"));
+//  }
+//
+//    }
 
 }
